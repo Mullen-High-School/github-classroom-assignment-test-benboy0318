d99415949fc6f1e127034c9269e359d128934c65
Mike McGuire <MoonshotMike@gmail.com>
2020-10-27 15:00:39 -0600

    Testing GitHub Classroom

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -1,8 +1,8 @@
 
 public class HelloWorld
 	{
-		public static void main(String[] args)
-			{
-				System.out.println("Hello Earth!");
-			}
+	public static void main(String[] args)
+		{
+			System.out.println("Hello Earth!");
+		}
 	}
